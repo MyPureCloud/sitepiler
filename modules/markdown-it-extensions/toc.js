c51b4ea997384cc972bd9008b80a2f17787fb8a7
Tim Smith <tim.j.smith@genesys.com>
2018-11-01 15:11:58 -0600

    adding TOC extension

---
+++ modules/markdown-it-extensions/toc.js
@@ -0,0 +1,60 @@
+'use strict';
+
+function getLine(state, line) {
+	var pos = state.bMarks[line] + state.blkIndent,
+		max = state.eMarks[line];
+
+	return state.src.substr(pos, max - pos);
+}
+
+module.exports = function(md, name, options) {
+	function toc(state, startLine, endLine/*, silent*/) {
+		var nextLine, token, lineText;
+
+		// Is TOC?
+		lineText = getLine(state, startLine);
+		if (lineText.trim().toLowerCase() !== ':::toc:::') return false;
+
+		// Move past the :::TOC::: line
+		nextLine = startLine + 1;
+		state.line = nextLine;
+		lineText = getLine(state, nextLine);
+
+		// Open list
+		token = state.push('bullet_list_open', 'ul', 1);
+		token.attrs = [ [ 'class', 'toc-list' ] ];
+
+		// Add content until end marker is reached
+		while(nextLine <= endLine) {
+			if (lineText) {
+				// Regex match all markdown headings (e.g. ### Heading Three)
+				let headingMatch = lineText.match(/^(#+)\s*(.+)/);
+				if (headingMatch) {
+
+					// Open list item
+					token = state.push('list_item_open', 'li', 1);
+					token.attrs = [ [ 'class', `toc-list-${headingMatch[1].length}` ] ];
+
+					// Add content
+					token = state.push('inline', '', 0);
+					token.content = `<a href="#${headingMatch[2].toLowerCase().trim().replace(/\s/g, '')}">${headingMatch[2]}</a>`;
+					token.children = [];
+
+					// Close list item
+					token = state.push('list_item_close', 'li', -1);
+				}
+			}
+
+			nextLine++;
+			lineText = getLine(state, nextLine);
+		}
+
+		// Close list
+		token = state.push('bullet_list_close', 'ul', -1);
+
+		return true;
+	}
+
+	// Add rule first
+	md.block.ruler.before('table', 'toc', toc);
+};
